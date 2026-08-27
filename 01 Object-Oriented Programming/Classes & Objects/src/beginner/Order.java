2a3d1fe1d68cbe5dc572214b7b366e83472b84c0
Serge <sajghaa@gmail.com>
2026-08-28 00:58:41 +0200

    update Order.java

--- 01 Object-Oriented Programming/Classes & Objects/src/beginner/Order.java
+++ 01 Object-Oriented Programming/Classes & Objects/src/beginner/Order.java
@@ -1,5 +1,31 @@
 package beginner;
 
+import java.util.UUID;
+
 public class Order {
+    
+    private String id;
+    private String itemName;
+    private int quantity;
+    private double pricePerUnit;
+    private double discountRate;
+
+    public Order(String itemName, int quantity, double pricePerUnit, double getTotalAmount) {
+
+        this.id = UUID.randomUUID().toString().substring(0, 8);
+        this.itemName = itemName;
+        this.quantity = quantity;
+        this.pricePerUnit = pricePerUnit;
+        this.discountRate = discountRate;
+    }
+
+    public double getTotalAmount(){
+        return this.quantity * this.pricePerUnit * (1 - this.discountRate);
+    }
+
+    @Override
+    public String toString(){
+        return "Order{id='" + id + "', item='" + itemName + "', total=" + getTotalAmount() + "}";
+    }
     
 }
