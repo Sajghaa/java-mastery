a9934cef3ffec7a777187cbdf5bdd1dba32615a3
Serge <sajghaa@gmail.com>
2026-09-03 21:14:08 +0200

    Update TaskManager.java

--- 11 Projects/task-manager/service/TaskManager.java
+++ 11 Projects/task-manager/service/TaskManager.java
@@ -1,5 +1,26 @@
 package service;
 
+import model.Task;
+import java.util.ArrayList;
+import java.util.List;
+
 public class TaskManager {
     
+    private List<Task> tasks;
+    private int nextId;
+
+    public TaskManager() {
+        tasks = new ArrayList<>();
+        nextId = 1;
+    }
+
+    public Task addTask(String title, int priority, String dueDate) {
+        Task newTask = new Task(nextId, title, priority, dueDate);
+        tasks.add(newTask);
+        return newTask;
+    }
+
+    public List<Task> getAllTasks() {
+        return tasks;
+    }
 }
