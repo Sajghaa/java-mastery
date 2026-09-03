b5c7d8003a62e6c28c0d0140aabcb31420197185
Serge <sajghaa@gmail.com>
2026-09-03 09:51:50 +0200

    update GuardClausesIntro.java

--- 00 Foundations/Conditionals/src/beginner/GuardClausesIntro.java
+++ 00 Foundations/Conditionals/src/beginner/GuardClausesIntro.java
@@ -2,4 +2,44 @@ package beginner;
 
 public class GuardClausesIntro {
     
+    public static void main(String[] args) {
+        
+        System.out.println("--- UGLY: Nested validation (DEEP) ---");
+        processOrderUgly(null, "pass123");
+
+        System.out.println("\n--- CLEAN: Guard clauses (FLAT) ---");
+        processOrderClean(null, "pass123");
+    }
+
+    public static void processOrderUgly(String email, String password) {
+        if (email != null) {
+            if (password != null) {
+                if (password.length() >= 6){
+                    System.out.println("Order processed successfully!");
+                } else {
+                    System.out.println("Password too short");
+                }
+            } else {
+                System.out.println("Password is required");
+            }
+        } else {
+            System.out.println("Email is required.");
+        }
+    }
+
+    public static void processOrderClean(String email, String password) {
+
+       
+        if (email == null) {
+            System.out.println("Email is required");
+            return;
+        }
+
+        if (password == null) {
+            System.out.println("Password is required.");
+            return;
+        }
+
+        System.out.println("Order processed successfully!");
+    }
 }
