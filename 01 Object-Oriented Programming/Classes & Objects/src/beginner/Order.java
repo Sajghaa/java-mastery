287a5cd1dff6b4c744622c839769524647087733
Serge <sajghaa@gmail.com>
2026-09-09 21:45:31 +0200

    Update Order.java

--- 01 Object-Oriented Programming/Classes & Objects/src/beginner/Order.java
+++ 01 Object-Oriented Programming/Classes & Objects/src/beginner/Order.java
@@ -3,29 +3,44 @@ package beginner;
 import java.util.UUID;
 
 public class Order {
-    
-    private String id;
-    private String itemName;
-    private int quantity;
-    private double pricePerUnit;
-    private double discountRate;
 
-    public Order(String itemName, int quantity, double pricePerUnit, double getTotalAmount) {
+     private String id;
+     private String itemName;
+     private int quantity;
+     private double pricePerUnit;
+
+     public  Order(String itemName, int quantity, double pricePerUnit) {
 
         this.id = UUID.randomUUID().toString().substring(0, 8);
+
         this.itemName = itemName;
         this.quantity = quantity;
         this.pricePerUnit = pricePerUnit;
-        this.discountRate = discountRate;
-    }
+     }
 
-    public double getTotalAmount(){
-        return this.quantity * this.pricePerUnit * (1 - this.discountRate);
-    }
+     public String getId() {
+        return id;
+     }
 
-    @Override
-    public String toString(){
-        return "Order{id='" + id + "', item='" + itemName + "', total=" + getTotalAmount() + "}";
-    }
-    
+     public String getItemName() {
+        return itemName;
+     }
+
+     public int getQuantity(){
+        return quantity;
+     }
+
+     public  double getPricePerUnit() {
+        return pricePerUnit;
+     }
+
+     public  double getTotalAmount() {
+        return this.quantity * this.pricePerUnit;
+     }
+
+     @Override 
+     public  String toString() {
+        return  "Order{id='" + id + "', item='" + itemName + "', total=" + getTotalAmount() + "}";
+     }
+
 }
